f63a5098f3d0f539d832080d0bbde5f7952a09bd
MatteoArt <mattrim8@gmail.com>
2023-07-11 23:03:36 +0200

    completo esercizio (incluso bonus)

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
 //recupero il container delle celle
 const containerSquare = document.querySelector(".container");
 
-//recupero il button
+//recupero l'input e il button
+const inputEl = document.querySelector(".input-select");
 const playBtn = document.querySelector(".btn-play");
 
 
 playBtn.addEventListener("click", function () {
-    //numero delle celle che compongono la griglia
-    let inputNum = 100;
-    console.log(createGrid(inputNum));
+    //numero delle celle che compongono la griglia (input select)
+    let inputNum = parseInt(inputEl.value);
+
+    //console.log(inputNum);
+
+    //array contenente le celle
+    let gridArr = createGrid(inputNum);
+
+    printing(containerSquare, gridArr);
 })
 
 
@@ -25,7 +32,7 @@ function createGrid(numberCells) {
     for (let i = 1; i <= numberCells; i++) {
         //creo cella
         let cell = document.createElement("div");
-        cell.innerHTML = `<span>${i}</span>`
+        cell.innerHTML = `<span class="number">${i}</span>`
         cell.classList.add("square"); //aggiungo classe per visualizzazione
         //calcolo dinamicamente la larghezza di ogni cella a partire dal numero per riga
         cell.style.flexBasis = `calc(100% / ${cellsPerRow})`;
@@ -38,7 +45,18 @@ function createGrid(numberCells) {
 
 //funzione che stamperà la griglia nel Dom
 function printing(container, list) {
+    //svuoto il container per evitare celle in eccesso
+    container.innerHTML = "";
+
     for (let i = 0; i < list.length; i++) {
-        
+        container.append(list[i]);
+
+        //dopo aver appeso il div applico un ascoltatore per ogni 
+        //elemento per ogni giro del ciclo --> ogni el avrà la sua
+        //funzione anonima che viene eseguita solo al click sull'elemento stesso
+        list[i].addEventListener("click", function () {
+            list[i].classList.toggle("success");
+            console.log(`Hai cliccato la cella ${i + 1}`);
+        })
     }
 }
